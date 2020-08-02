557dcbac0d20980bd735a559f6c18ee702211bbe
xilen0x <carlos.astorgax@gmail.com>
2020-08-02 18:34:51 -0400

    finished create and delete note

--- src/components/CreateNotes.jsx
+++ src/components/CreateNotes.jsx
@@ -8,24 +8,36 @@ export default class CreateNotes extends Component {
     state = {
         users: [],
         userSelected: '',
-        title:'',
+        title: '',
         content: '',
         date: new Date()
     }
 
     async componentDidMount() {
         const res = await axios.get('http://localhost:4000/api/users');
-        this.setState({ users: res.data.map(user => user.username) })
-        console.log(this.state.users);
+        this.setState({
+            users: res.data.map(user => user.username),
+            userSelected: res.data[0].username
+        })
+        //console.log(this.state.users);
     }
 
-    onSubmit = (e) => {
+    onSubmit = async (e) => {
         //console.log(this.state.title, this.state.content);
         e.preventDefault();
+        const newNote = {
+            title: this.state.title,
+            content: this.state.content,
+            date: this.state.date,
+            author: this.state.userSelected
+        };
+        await axios.post('http://localhost:4000/api/notes', newNote);
+        //console.log(res);
+        window.location.href = '/';
     }
 
     onInputChange = e => {
-        console.log(e.target.name, e.target.value);
+        //console.log(e.target.name, e.target.value);
         this.setState({
             [e.target.name]: e.target.value /* dependiendo del nombre recibido, cambiará el state de aquel. */
         })
@@ -55,21 +67,21 @@ export default class CreateNotes extends Component {
                     </div>
                     <div className="form-group">
                         <input
-                         type="text"
-                         className="form-control"
-                         placeholder="Title"
-                         name="title"
-                         onChange={this.onInputChange}
-                         required />
+                            type="text"
+                            className="form-control"
+                            placeholder="Title"
+                            name="title"
+                            onChange={this.onInputChange}
+                            required />
                     </div>
                     <div className="form-group">
                         <textarea
-                         name="content"
-                         className="form-control"
-                         placeholder="Content"
-                         onChange={this.onInputChange}      
-                         required>
-                            </textarea>
+                            name="content"
+                            className="form-control"
+                            placeholder="Content"
+                            onChange={this.onInputChange}
+                            required>
+                        </textarea>
                     </div>
 
                     <div className="form-group">
